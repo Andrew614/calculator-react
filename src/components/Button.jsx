ed8031f1fa4203d32d12cf9bf805b48bddf0d89c
Andrew614 <azhang213@gmail.com>
2019-08-20 19:59:00 -0400

    create calculator buttons/ design

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css';
+
+const isOperator = val => {
+    return !isNaN(val) || val === '.' || val === '='
+}
+
+export const Button = props => (
+    <div className={`button-wrapper ${
+        isOperator(props.children) ? null : "operator"}`}>
+        {props.children}
+    </div>
+)
