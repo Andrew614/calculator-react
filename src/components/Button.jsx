e912f2836d73adb32e424a63478d54c7b3e8f095
Andrew614 <azhang213@gmail.com>
2019-08-20 20:53:15 -0400

    functional calculator app

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,8 @@ const isOperator = val => {
 
 export const Button = props => (
     <div className={`button-wrapper ${
-        isOperator(props.children) ? null : "operator"}`}>
+        isOperator(props.children) ? null : "operator"}`}
+        onClick={() => props.handleClick(props.children)}>
         {props.children}
     </div>
 )
